45ba172e06c3344f7c632654b6410f7d1453ebab
andualem23 <andualem2326@gmail.com>
2023-05-14 22:37:48 +0300

    add register form

--- moneytracker/moneytracker/settings.py
+++ moneytracker/moneytracker/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dotenv
+
 dotenv.read_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,3 +137,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

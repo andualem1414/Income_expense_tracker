ab4efaddb4e68b7de286188cea0f4d8206d9dd67
andualem23 <andualem2326@gmail.com>
2023-05-15 15:47:48 +0300

    added email validation and password hide button

--- moneytracker/authentication/views.py
+++ moneytracker/authentication/views.py
@@ -3,9 +3,25 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
+
 
 # Create your views here.
 
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email format not correct'}, status=400)
+        
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'Sorry email exists, Try new one.'}, status=409)
+
+        return JsonResponse({'email_valid':True})
+    
+    
 class UsernameValidationView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -14,10 +30,12 @@ class UsernameValidationView(View):
         if not str(username).isalnum():
             return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'}, status=400)
         
-        if User.objects.filter(username= username).exists():
+        if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'Sorry username exists, Try new one.'}, status=409)
 
         return JsonResponse({'username_valid':True})
+    
+
     
 class RegistrationView(View):
     def get(self, request):
--- moneytracker/moneytracker/static/js/register.js
+++ moneytracker/moneytracker/static/js/register.js
@@ -1,8 +1,12 @@
 const usernameField = document.querySelector("#usernameField");
 const feedbackArea = document.querySelector(".invalid-feedback");
+const usernameSuccess = document.querySelector(".usernameSuccess");
 
 usernameField.addEventListener("keyup", (e) => {
     const usernameVal = e.target.value;
+
+    usernameSuccess.style.display = "block";
+    usernameSuccess.textContent = `Checking ${usernameVal} ...`;
 
     usernameField.classList.remove("is-invalid");
     feedbackArea.style.display = "none";
@@ -16,11 +20,63 @@ usernameField.addEventListener("keyup", (e) => {
         })
             .then((res) => res.json())
             .then((data) => {
+                usernameSuccess.style.display = "none";
                 if (data.username_error) {
                     usernameField.classList.add("is-invalid");
                     feedbackArea.style.display = "block";
                     feedbackArea.innerHTML = `<p>${data.username_error}</p>`;
                 }
             });
+    } else {
+        usernameSuccess.style.display = "none";
     }
 });
+
+const emailField = document.querySelector("#emailField");
+const emailFeedbackArea = document.querySelector(".emailFeedbackArea");
+const emailSuccess = document.querySelector(".emailSuccess");
+
+emailField.addEventListener("keyup", (e) => {
+    emailVal = e.target.value;
+
+    emailSuccess.style.display = "block";
+    emailSuccess.textContent = `Checking ${emailVal} ...`;
+
+    emailField.classList.remove("is-invalid");
+    emailFeedbackArea.style.display = "none";
+
+    if (emailVal.length > 0) {
+        fetch("/authentication/validate-email", {
+            body: JSON.stringify({
+                email: emailVal,
+            }),
+
+            method: "POST",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                emailSuccess.style.display = "none";
+                if (data.email_error) {
+                    emailField.classList.add("is-invalid");
+                    emailFeedbackArea.innerHTML = "<p>Email not correct </p>";
+                    emailFeedbackArea.style.display = "block";
+                }
+            });
+    } else {
+        emailSuccess.style.display = "none";
+    }
+});
+
+showPasswordToggle = document.querySelector(".showPasswordToggle");
+passwordField = document.querySelector("#passwordField");
+
+const handelToggleInput = (e) => {
+    if (showPasswordToggle.textContent == "SHOW") {
+        showPasswordToggle.textContent = "HIDE";
+        passwordField.setAttribute("type", "text");
+    } else {
+        showPasswordToggle.textContent = "SHOW";
+        passwordField.setAttribute("type", "password");
+    }
+};
+showPasswordToggle.addEventListener("click", handelToggleInput);
